a67f590db450d232f2c02bb9015d3da1f5c8e883
Taaqif <taaqifpeck@gmail.com>
2017-08-10 11:46:22 +0800

    Added All FIles

---
+++ Assignment2.java
@@ -0,0 +1,16 @@
+
+package assignment.pkg2;
+
+/**
+ * Class which contains the main function
+ * @author Taaqif
+ */
+public class Assignment2 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TreeGUI treeGUI = new TreeGUI();
+    } 
+}
